dee5fb6b9429f445d1ad4f8f0412dc4c7f1c9f83
Tiago Souza Garcia <garciats@saude.sc.gov.br>
2024-01-04 17:25:48 -0300

    Primeiro commit - página de receitas

---
+++ src/lib/components/Tag.svelte
@@ -0,0 +1,35 @@
+<script>
+  //Prop para definir a classe. Ele será usada dinamicamente.
+  export let ativa = false;
+  export let selecionavel = false;
+
+  function aoClicar() {
+    if (selecionavel) {
+      ativa = !ativa;
+    }
+  }
+</script>
+
+<div class="tag" class:ativa on:click={aoClicar}>
+  <slot />
+</div>
+
+<style>
+  .tag {
+    height: 48px;
+    border-radius: 8px;
+    padding: 1rem;
+    text-align: center;
+    color: var(--cor-texto-principal);
+    background-color: var(--cinza);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .ativa {
+    color: var(--branco);
+    background-color: var(--laranja);
+  }
+</style>
---
+++ src/routes/receitas/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+  <title>Super Cozinha | Receitas</title>
+</svelte:head>
+
+<p>Aqui vem as receitas</p>
+
+<a href="/">Voltar para a página principal</a>
